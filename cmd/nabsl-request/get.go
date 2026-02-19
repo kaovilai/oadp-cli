bde9bf30ba369070dbdbb009cea35b961ff3bd6d
Joseph <jvaikath@redhat.com>
2026-02-19 14:37:00 -0800

    Linting fix
    Signed-off-by: Joseph <jvaikath@redhat.com>

--- cmd/nabsl-request/get.go
+++ cmd/nabsl-request/get.go
@@ -123,9 +123,7 @@ func (o *GetOptions) Run(c *cobra.Command, f client.Factory) error {
 
 	// List all requests in admin namespace
 	var requestList nacv1alpha1.NonAdminBackupStorageLocationRequestList
-	var err error
-
-	err = o.client.List(context.Background(), &requestList, &kbclient.ListOptions{
+	var err error = o.client.List(context.Background(), &requestList, &kbclient.ListOptions{
 		Namespace: adminNS,
 	})
 
